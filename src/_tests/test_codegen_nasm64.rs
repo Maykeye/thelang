fb55bf435b17f4dc4f9811f3655d4f230d8eae4b
Maykeye <Maykeye@users.noreply.github.com>
2025-05-23 23:52:56 +0600

    Nasm64: added test_nested_block

--- src/_tests/test_codegen_nasm64.rs
+++ src/_tests/test_codegen_nasm64.rs
@@ -2,11 +2,15 @@ use crate::{codegen::CodeGen, ir::IR};
 
 use super::CodeGenNasm64;
 
+fn compile_to_text(source: &str) -> String {
+    let ir = IR::from_thelan(source).unwrap();
+    let nasm = CodeGenNasm64::from_ir(&ir).unwrap();
+    nasm.to_text().unwrap().join("\n")
+}
+
 #[test]
 fn test_triple_ret() {
-    let ir = IR::from_thelan(" fn main() {\nreturn;return();\nreturn } ").unwrap();
-    let nasm = CodeGenNasm64::from_ir(&ir).unwrap();
-    let text = nasm.to_text().unwrap().join("\n");
+    let text = compile_to_text(" fn main() {\nreturn;return();\nreturn } ");
     let expected = "\
 global main
 
@@ -16,6 +20,29 @@ main:
 .b0:
     ret
     ret
+    ret
+";
+    assert_eq!(text, expected);
+}
+
+#[test]
+fn test_nested_block() {
+    let text = compile_to_text("fn main() {{{}}}");
+    let expected = "\
+global main
+
+section .text
+
+main:
+.b0:
+    call .b1
+    ret
+
+.b1:
+    call .b2
+    ret
+
+.b2:
     ret
 ";
     assert_eq!(text, expected);
